a7404c06cb9c57356a444efc99d59efc9debdfa3
KangWon LEE <kangwon@gmail.com>
2024-05-25 21:29:06 +0900

    update my_code_here.py

--- my_code_here.py
+++ my_code_here.py
@@ -1,3 +1,19 @@
 # Please complete this file
 # 이 파일을 완성하시오
-def f(
+def linear_damping_force(
+
+
+def nonlinear_damping_force(
+
+
+def linear_slope(
+
+
+def nonlinear_slope(
+
+
+def linear_solution(
+
+
+def nonlinear_solution(
+
